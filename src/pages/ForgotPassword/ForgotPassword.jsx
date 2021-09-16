0a1a81ed9fececebff62a5e24868fefb66498676
TH902664 <ttqk1999@gmail.com>
2021-09-16 21:58:34 +0700

    Test changed

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -2,11 +2,18 @@ import TextField from "src/components/TextField/TextField";
 
 const ForgotPassword = () => {
   return (
-    <TextField
-      label="Email address"
-      placeholder="Please Enter User Name or Email"
-      type="email"
-    />
+    <>
+      <TextField
+        label="Email address"
+        placeholder="Please Enter User Name or Email"
+        type="email"
+      />
+      <TextField
+        label="Email address"
+        placeholder="Please Enter User Name or Email"
+        type="password"
+      />
+    </>
   );
 };
 
